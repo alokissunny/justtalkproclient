dedec35b46e7bc92647fdb36700bcf19fb462427
alokissunny <bit.srivastava@gmail.com>
2018-04-15 11:01:21 +0530

    dummy files remove

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -37,29 +37,6 @@ const routes: Routes = [{
     loadChildren: './calender/calender.module#CalenderModule',
   },
   {
-    path: 'ui-features',
-    loadChildren: './ui-features/ui-features.module#UiFeaturesModule',
-  }, {
-    path: 'components',
-    loadChildren: './components/components.module#ComponentsModule',
-  }, {
-    path: 'maps',
-    loadChildren: './maps/maps.module#MapsModule',
-  }, {
-    path: 'charts',
-    loadChildren: './charts/charts.module#ChartsModule',
-  }, {
-    path: 'editors',
-    loadChildren: './editors/editors.module#EditorsModule',
-  }, {
-    path: 'forms',
-    loadChildren: './forms/forms.module#FormsModule',
-  }, {
-    path: 'tables',
-    loadChildren: './tables/tables.module#TablesModule',
-  }
-    ,
-  {
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full',
